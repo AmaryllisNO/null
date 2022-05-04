a546bf97e3dc4f7a86607fa2c22348b09ab8ceda
Alexis <amaryllisno@gmail.com>
2022-05-04 08:11:47 +0200

    project setup

---
+++ js/index.js
@@ -0,0 +1,4 @@
+console.log('hello world update?????');
+var listItems = document.querySelectorAll('li');
+console.log(listItems);
+//# sourceMappingURL=index.js.map
---
+++ js/index.ts
@@ -0,0 +1,4 @@
+console.log('hello world update?????');
+let listItems = document.querySelectorAll('li');
+
+console.log(listItems);
