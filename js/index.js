e13620a413a515a6ae8a46bc238c8c4da9cc659b
Alexis <amaryllisno@gmail.com>
2022-05-05 13:39:44 +0200

    finish

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
-var listItems = document.querySelectorAll('li');
 var itemList = document.querySelector('#insert');
+var albumInsert = document.querySelector('#insert2');
 var textInput = document.querySelector('#textinput');
 console.log(textInput);
 var titleArray = [
-    "<li class='blooditem'>Secrets Are Secrets For A Reason</li>",
-    "<li class='blooditem'>Our Toll</li>",
-    "<li class='blooditem'>Ophanim</li>",
-    "<li class='blooditem'>The Lines Between</li>",
-    "<li class='blooditem'>The Ever Infinite Nothing</li>",
+    "<li class='blooditem'>secrets are secrets for a reason</li>",
+    "<li class='blooditem'>our toll</li>",
+    "<li class='blooditem'>ophanim</li>",
+    "<li class='blooditem'>the lines between</li>",
+    "<li class='blooditem'>the ever infinite nothing</li>",
+    "<li class='blooditem claw'>clawatmyskindigatmyfaceripoutmyhair</li>",
 ];
 var urlArray = [
     "<a href ='./p/sasfar.html'>",
@@ -15,6 +16,7 @@ var urlArray = [
     "<a href ='./p/ophanim.html'>",
     "<a href ='./p/lines.html'>",
     "<a href ='./p/nothing.html'>",
+    "<a href ='./p/erase.html'>",
 ];
 //var list = "<ul>\n    <li><a href='./pages/01.html'> 00</a></li>\n    <li class='blooditem'>Secrets Are Secrets For A Reason</li>\n    <li class='blooditem'>Our Toll</li>\n    <li class='blooditem'>Ophanim</li>\n    <li class='blooditem'>The Lines Between</li>\n    <li class='blooditem'>The Ever Infinite Nothing</li>\n</ul>";
 var titleList = '<ul>';
@@ -28,10 +30,17 @@ function populateTitleList() {
     itemList.innerHTML = titleList;
 }
 function toggleTitleStyle(titleIndex) {
-    titleArray[titleIndex] = titleArray[titleIndex].replace(" class='blooditem'", '');
-    titleArray[titleIndex] = titleArray[titleIndex].replace('<li>', '<li>' + urlArray[titleIndex]);
-    titleArray[titleIndex] = titleArray[titleIndex].replace('</li>', '</a></li>');
-    populateTitleList();
+    if (titleIndex === 5) {
+        titleArray[titleIndex] = titleArray[titleIndex].replace(" class='blooditem claw'", '');
+        titleArray[titleIndex] = titleArray[titleIndex].replace('<li>', '<li>' + urlArray[titleIndex]);
+        populateTitleList();
+    }
+    else {
+        titleArray[titleIndex] = titleArray[titleIndex].replace(" class='blooditem'", '');
+        titleArray[titleIndex] = titleArray[titleIndex].replace('<li>', '<li>' + urlArray[titleIndex]);
+        titleArray[titleIndex] = titleArray[titleIndex].replace('</li>', '</a></li>');
+        populateTitleList();
+    }
 }
 populateTitleList();
 itemList.innerHTML = titleList;
@@ -60,24 +69,36 @@ function checkInput() {
         window.localStorage.setItem('the ever infinite nothing', 'true');
         toggleTitleStyle(4);
     }
+    if (textInputValue === 'clawatmyskindigatmyfaceripoutmyhair') {
+        window.localStorage.setItem('clawatmyskindigatmyfaceripoutmyhair', 'true');
+        toggleTitleStyle(5);
+    }
+    if (window.localStorage.getItem('the ever infinite nothing') &&
+        window.localStorage.getItem('the lines between') &&
+        window.localStorage.getItem('ophanim') &&
+        window.localStorage.getItem('our toll') &&
+        window.localStorage.getItem('secrets are secrets for a reason')) {
+        console.log('all titles found');
+        albumInsert.innerHTML = "<a href='https://null00.bandcamp.com/album/secrets-are-secrets-for-a-reason' target=\u201D_blank\u201D><img class='album' src='../content/img/secrets.jpg'></img>\n    <p>we're all human -  even if some say otherwise</p></a>";
+    }
+    if (window.localStorage.getItem('clawatmyskindigatmyfaceripoutmyhair')) {
+        albumInsert.innerHTML = "<a href='https://null00.bandcamp.com/album/secrets-are-secrets-for-a-reason' target=\u201D_blank\u201D><img class='album' src='../content/img/secrets.jpg'></img>\n    <p>we're all human -  even if some say otherwise</p></a>";
+    }
 }
-if (localStorage.getItem('ophanim')) {
-}
-localStorage.setItem('void', '1');
-/* document.addEventListener('contextmenu', function (e) {
-  e.preventDefault();
-}); */
+// localStorage.setItem('void', '1');
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
 var disableselect = function (e) {
     return false;
 };
 /* document.onselectstart = disableselect;
-document.onmousedown = disableselect;
- */
-//# sourceMappingURL=index.js.map
+document.onmousedown = disableselect; */
+// sourceMappingURL=index.js.map
 document.onkeydown = function (e) {
-    if (event.keyCode == 123) {
-        return false;
-    }
+    /*  if (event.keyCode == 123) {
+      return false;
+    } */
     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
         return false;
     }
--- js/index.ts
+++ js/index.ts
@@ -1,5 +1,5 @@
-var listItems = document.querySelectorAll('li');
 var itemList = document.querySelector('#insert');
+let albumInsert = document.querySelector('#insert2');
 var textInput = document.querySelector('#textinput');
 console.log(textInput);
 
@@ -8,8 +8,10 @@ var titleArray = [
   "<li class='blooditem'>our toll</li>",
   "<li class='blooditem'>ophanim</li>",
   "<li class='blooditem'>the lines between</li>",
-  "<li class='blooditem'>clawatmyskindigatmyfaceripoutmyhair</li>",
+  "<li class='blooditem'>the ever infinite nothing</li>",
+  "<li class='blooditem claw'>clawatmyskindigatmyfaceripoutmyhair</li>",
 ];
+
 var urlArray = [
   "<a href ='./p/sasfar.html'>",
   "<a href ='./p/toll.html'>",
@@ -25,34 +27,57 @@ var titleList = '<ul>';
 function populateTitleList() {
   titleList = '<ul>';
   titleList += "<li><a href='./p/01.html'> 00</a></li>";
+
   for (let i = 0; i < titleArray.length; i++) {
     titleList += titleArray[i];
   }
+
   titleList += '</ul>';
   itemList.innerHTML = titleList;
 }
 
 function toggleTitleStyle(titleIndex) {
-  titleArray[titleIndex] = titleArray[titleIndex].replace(
-    " class='blooditem'",
-    ''
-  );
-  titleArray[titleIndex] = titleArray[titleIndex].replace(
-    '<li>',
-    '<li>' + urlArray[titleIndex]
-  );
-  titleArray[titleIndex] = titleArray[titleIndex].replace('</li>', '</a></li>');
-  populateTitleList();
+  if (titleIndex === 5) {
+    titleArray[titleIndex] = titleArray[titleIndex].replace(
+      " class='blooditem claw'",
+      ''
+    );
+
+    titleArray[titleIndex] = titleArray[titleIndex].replace(
+      '<li>',
+      '<li>' + urlArray[titleIndex]
+    );
+
+    populateTitleList();
+  } else {
+    titleArray[titleIndex] = titleArray[titleIndex].replace(
+      " class='blooditem'",
+      ''
+    );
+
+    titleArray[titleIndex] = titleArray[titleIndex].replace(
+      '<li>',
+      '<li>' + urlArray[titleIndex]
+    );
+    titleArray[titleIndex] = titleArray[titleIndex].replace(
+      '</li>',
+      '</a></li>'
+    );
+    populateTitleList();
+  }
 }
 
 populateTitleList();
 
 itemList.innerHTML = titleList;
+
 textInput.addEventListener('keyup', checkInput);
+
 function checkInput() {
   var textInputValue = (<HTMLInputElement>document.querySelector('#textinput'))
     .value;
   console.log(textInputValue);
+
   if (textInputValue === 'secrets are secrets for a reason') {
     window.localStorage.setItem('secrets are secrets for a reason', 'true');
     toggleTitleStyle(0);
@@ -73,25 +98,48 @@ function checkInput() {
     window.localStorage.setItem('the ever infinite nothing', 'true');
     toggleTitleStyle(4);
   }
+  if (textInputValue === 'clawatmyskindigatmyfaceripoutmyhair') {
+    window.localStorage.setItem('clawatmyskindigatmyfaceripoutmyhair', 'true');
+    toggleTitleStyle(5);
+  }
+
+  if (
+    window.localStorage.getItem('the ever infinite nothing') &&
+    window.localStorage.getItem('the lines between') &&
+    window.localStorage.getItem('ophanim') &&
+    window.localStorage.getItem('our toll') &&
+    window.localStorage.getItem('secrets are secrets for a reason')
+  ) {
+    console.log('all titles found');
+    albumInsert.innerHTML = `<a href='https://null00.bandcamp.com/album/secrets-are-secrets-for-a-reason' target=”_blank”><img class='album' src='../content/img/secrets.jpg'></img>
+    <p>we're all human -  even if some say otherwise</p></a>`;
+  }
+
+  if (window.localStorage.getItem('clawatmyskindigatmyfaceripoutmyhair')) {
+    albumInsert.innerHTML = `<a href='https://null00.bandcamp.com/album/secrets-are-secrets-for-a-reason' target=”_blank”><img class='album' src='../content/img/secrets.jpg'></img>
+    <p>we're all human -  even if some say otherwise</p></a>`;
+  }
 }
-if (localStorage.getItem('ophanim')) {
-}
-localStorage.setItem('void', '1');
+
+// localStorage.setItem('void', '1');
+
 document.addEventListener('contextmenu', function (e) {
   e.preventDefault();
 });
+
 var disableselect = function (e) {
   return false;
 };
+
 /* document.onselectstart = disableselect;
-document.onmousedown = disableselect;
- */
-//# sourceMappingURL=index.js.map
+document.onmousedown = disableselect; */
+
+// sourceMappingURL=index.js.map
 
 document.onkeydown = function (e) {
-  if (event.keyCode == 123) {
+  /*  if (event.keyCode == 123) {
     return false;
-  }
+  } */
   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
     return false;
   }
